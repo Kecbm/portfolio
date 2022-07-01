53360349792d958545017ccd688564b9b58a0a7f
Klecianny Melo <kleciannymelo@gmail.com>
2022-07-01 18:24:27 -0300

    retirei a seta

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Seta from '../assets/Seta.png';
 import '../css/Home.css';
 
 class Home extends Component {
@@ -14,7 +13,6 @@ class Home extends Component {
               Estudante de desenvolvimento web full stack e embaixadora da Turma XP - Trybe
             </p>
             <a target="_blank" href="https://www.cvkeep.com/cv/7e863938f1cc31f1755d834316835807"><button className="button">CURRICULO</button></a>
-            <img src={ Seta } className="seta" alt="seta" />
           </section>
         </div>
       </div>
