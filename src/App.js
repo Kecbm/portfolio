ca40c1db548b20da5e4504ceb048327183c582c4
Klecianny Melo <kleciannymelo@gmail.com>
2023-04-20 16:48:03 -0300

    :sparkles: estrutura de rotas

--- src/App.js
+++ src/App.js
@@ -5,15 +5,21 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
 import Footer from './pages/Footer';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
-      <About />
-      <Projects />
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={ <Home /> } />
+          <Route exact path="/about" element={ <About /> } />
+          <Route exact path="/projects" element={ <Projects /> } />
+          {/* <Route path="*" element={ NotFound } /> */}
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
