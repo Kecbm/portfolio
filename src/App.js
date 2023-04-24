74e82542061b27bc3f89c793414ea3b9cb321ed7
Klecianny Melo <kleciannymelo@gmail.com>
2023-04-24 17:17:10 -0300

    :sparkles: nova rota: /contacts

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
 import Footer from './pages/Footer';
+import Contact from './pages/Contact';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
           <Route exact path="/" element={ <Home /> } />
           <Route exact path="/about" element={ <About /> } />
           <Route exact path="/projects" element={ <Projects /> } />
+          <Route exact path="/contact" element={ <Contact /> } />
           {/* <Route path="*" element={ NotFound } /> */}
         </Routes>
         <Footer />
