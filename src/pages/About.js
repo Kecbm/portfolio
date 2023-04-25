1e1babf4dfca82bf7a24bac646a038e1ea77104b
Klecianny Melo <kleciannymelo@gmail.com>
2023-04-25 16:46:11 -0300

    :sparkles: ajustes em About

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,71 +1,75 @@
 import React, { Component } from 'react';
+import secondAvatar from '../assets/Avatar2.png';
 import '../css/About.css';
 
 class About extends Component {
   render() {
     return (
-      <div>
-        <div className="about">
-          <section>
-            <div>
-              <h1 className="titleAbout"><a name="sobre">SOBRE</a></h1>
-              <p className="text-about"> 
-              Olá, Eu sou Klecianny Melo, sou Desenvolvedora Web Full Stack, tenho 27 anos e estou localizada em Garanhuns - Pernambuco.
-              {' '}
-              Sou apaixonada por tecnologia e admiro a transformação que o ambiente digital proporciona ao mundo. Sinta-se à vontade para entrar em contato comigo.
-              </p>
-            </div>
-            <div className="skills">
-              <h1 className="titleAbout">SKILLS</h1>
-              <img title="C" alt="C" height="60" width="60" src="https://raw.githubusercontent.com/jmnote/z-icons/master/svg/c.svg" />
-              <img title="Java" alt="Java" height="60" width="60" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg" />
-              <img title="HTML" alt="HTML" height="60" width="60" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg" />
-              <img title="CSS" alt="CSS" height="60" width="60" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg" />
-              <img title="JavaScript" alt="JavaScript" height="60" width="60" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg" />
-              <img title="TypeScript" alt="TypeScript" height="60" width="60" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg" />
-              <img title="Python" alt="Python" height="60" width="60" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" />
-              <img title="C Sharp" alt="C Sharp" width="60" height="60" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/csharp/csharp-original.svg" />
-              <img title="Linux" alt="Linux" width="60" height="60" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/linux/linux-original.svg" />
-              <img title="Debian" alt="Debian" width="60" height="60" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/debian/debian-original.svg" />
-              <img title="Terminal" alt="Terminal" width="60" height="60" src="https://cdn.svgporn.com/logos/terminal.svg" />
-              <img title="Git" alt="Git" width="60" height="60" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" />
-              <img title="GitHub" alt="GitHub" width="60" height="60" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" />
-              <img title="NPM" alt="NPM" width="60" height="60" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg" />
-              <img title="Eslint" alt="Eslint" width="60" height="60" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/eslint/eslint-original.svg" />
-              <img title="Vscode" alt="Vscode" width="60" height="60" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg" />
-              <img title="Vercel" alt="Vercel" width="60" height="60" src="https://www.svgrepo.com/show/327408/logo-vercel.svg" />
-              <img title="Heroku" alt="Heroku" width="60" height="60" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/heroku/heroku-plain.svg" />
-              <img title="Trello" alt="Trello" width="60" height="60" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/trello/trello-plain.svg" />
-              <img title="Jest" alt="Jest" width="60" height="60" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/jest/jest-plain.svg" />
-              <img title="Docker" alt="Docker" width="60" height="60" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original.svg" />
-              <img title="Pytest" alt="Pytest" width="60" height="60" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/pytest/pytest-original.svg" />
-              <img title="React" alt="React" width="60" height="60" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg" />
-              <img title="Redux" alt="Redux" width="60" height="60" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg" />
-              <img title="React Testing Library" alt="React Testing Library" width="60" height="60" src="https://testing-library.com/img/logo-large.png" />
-              <img title="Node.JS" alt="Node.JS" width="60" height="60" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" />
-              <img title="Express" alt="Express" width="60" height="60" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" />
-              <img title="MySQL" alt="MySQL" width="60" height="60" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg" />
-              <img title="MongoDB" alt="MongoDB" width="60" height="60" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" />
-              <img title="Mocha" alt="Mocha" width="60" height="60" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mocha/mocha-plain.svg" />
-              <img title="Sequelize" alt="Sequelize" width="60" height="60" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sequelize/sequelize-original.svg" />
-              <img title="PHP" alt="PHP" width="60" height="60" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg" />
-           </div>
-           <div className="list-about">
-            <h1 className="titleAbout">Atuação Profissional</h1>
-            <ul className="text-about">
-              <li>🏦 Software Engineer 1 - XP Inc. (09/2022 - 03/2023)</li>
-              <li>🎷 Saxofonista na Orquestra Manoel Rabelo - Prefeitura Municipal de Garanhuns (01/2017 - 12/2020)</li>
-            </ul>
-           </div>
-           <div className="list-about">
-            <h1 className="titleAbout">Formação Acadêmica</h1>
-            <ul className="text-about">
-              <li>🖥 Desenvolvimento Web Full Stack - Trybe (2021 - 2022)</li>
-              <li>👩🏽‍🔬 Engenharia de Alimentos - UFAPE (2014 - 2021)</li>
-            </ul>
-           </div>
-          </section>
-        </div>
+      <div className="about">
+        <section>
+          <div>
+            <h1 className="titleAbout"><a name="sobre">SOBRE</a></h1>
+            <p className="text-about"> 
+            Olá, Eu sou Klecianny Melo, sou Desenvolvedora Web Full Stack, tenho 27 anos e estou localizada em Garanhuns - Pernambuco.
+            {' '}
+            Sou apaixonada por tecnologia e admiro a transformação que o ambiente digital proporciona ao mundo. Sinta-se à vontade para entrar em contato comigo.
+            </p>
+          </div>
+          <div className="skills">
+            <h1 className="titleAbout">SKILLS</h1>
+            <img title="C" alt="C" src="https://raw.githubusercontent.com/jmnote/z-icons/master/svg/c.svg" />
+            <img title="Java" alt="Java" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg" />
+            <img title="HTML" alt="HTML" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg" />
+            <img title="CSS" alt="CSS" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg" />
+            <img title="JavaScript" alt="JavaScript" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg" />
+            <img title="TypeScript" alt="TypeScript" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg" />
+            <img title="Python" alt="Python" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" />
+            <img title="C Sharp" alt="C Sharp" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/csharp/csharp-original.svg" />
+            <img title="Kotlin" alt="Kotlin" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/kotlin/kotlin-original.svg" />
+            <img title="Linux" alt="Linux" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/linux/linux-original.svg" />
+            <img title="Debian" alt="Debian" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/debian/debian-original.svg" />
+            <img title="Android" alt="Android" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/android/android-original.svg" />
+            <img title="Terminal" alt="Terminal" src="https://cdn.svgporn.com/logos/terminal.svg" />
+            <img title="Git" alt="Git" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" />
+            <img title="GitHub" alt="GitHub" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" />
+            <img title="NPM" alt="NPM" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg" />
+            <img title="Eslint" alt="Eslint" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/eslint/eslint-original.svg" />
+            <img title="Vscode" alt="Vscode" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg" />
+            <img title="Adroid Studio" alt="Adroid Studio" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/androidstudio/androidstudio-original.svg" />
+            <img title="Vercel" alt="Vercel" src="https://www.svgrepo.com/show/327408/logo-vercel.svg" />
+            <img title="Heroku" alt="Heroku" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/heroku/heroku-plain.svg" />
+            <img title="Trello" alt="Trello" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/trello/trello-plain.svg" />
+            <img title="Jest" alt="Jest" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/jest/jest-plain.svg" />
+            <img title="Docker" alt="Docker" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original.svg" />
+            <img title="Pytest" alt="Pytest" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/pytest/pytest-original.svg" />
+            <img title="React" alt="React" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg" />
+            <img title="Redux" alt="Redux" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg" />
+            <img title="React Testing Library" alt="React Testing Library" src="https://testing-library.com/img/logo-large.png" />
+            <img title="Node.JS" alt="Node.JS" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" />
+            <img title="Express" alt="Express" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" />
+            <img title="MySQL" alt="MySQL" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg" />
+            <img title="MongoDB" alt="MongoDB" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" />
+            <img title="Mocha" alt="Mocha" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mocha/mocha-plain.svg" />
+            <img title="Sequelize" alt="Sequelize" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sequelize/sequelize-original.svg" />
+            <img title="PHP" alt="PHP" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg" />
+          </div>
+          <div className="list-about">
+          <h1 className="titleAbout">Atuação Profissional</h1>
+          <ul className="text-about">
+            <li>🏦 Software Engineer 1 - XP Inc. (09/2022 - 03/2023)</li>
+            <li>🎷 Saxofonista na Orquestra Manoel Rabelo - Prefeitura Municipal de Garanhuns (01/2017 - 12/2020)</li>
+          </ul>
+          </div>
+          <div className="list-about">
+          <h1 className="titleAbout">Formação Acadêmica</h1>
+          <ul className="text-about">
+            <li>📱 Grupo de Estudos e Desenvolvimento Android - Google Developer Group Poços de Caldas (04/2023)</li>
+            <li>🖥 Desenvolvimento Web Full Stack - Trybe (10/2021 - 12/2022)</li>
+            <li>👩🏽‍🔬 Engenharia de Alimentos - Universidade Federal do Agreste de Pernambuco (01/2014 - 05/2021)</li>
+          </ul>
+          </div>
+          <img alt="Avatar" src={secondAvatar}  id="avatar-home" />
+        </section>
       </div>
     );
   }
