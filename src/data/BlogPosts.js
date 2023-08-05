f556bf1dedb0ced17caca447e7f1250015c80215
Klecianny Melo <kleciannymelo@gmail.com>
2023-08-04 22:29:38 -0300

    :construction: montando o array de blog posts

--- src/data/BlogPosts.js
+++ src/data/BlogPosts.js
@@ -1,7 +1,28 @@
-import Calculadora from '../assets/01.Calculadora.png';
-
 const blogPosts = [
-  { image: Calculadora, title: 'Como ser uma pessoa desenvolvedora fora da curva por Sibelius Seraphini', description: 'Dicas e conselhos para se destacar como desenvolvedor, abordando habilidades técnicas, mindset, aprendizado contínuo e networking', url: 'https://codepen.io/klecianny-melo/pen/bGRYLME' }
+  {
+    image: <img width="100" height="100" src="https://img.icons8.com/stickers/100/bot.png" alt="Bot"/>,
+    title: 'ChatGPT: o que é, como funciona, limitações, ferramentas e atualizações GPT-5',
+    description: 'Entenda o modelo de linguagem baseado em IA desenvolvido pela OpenAI e suas capacidades de geração de respostas relevantes.',
+    url: 'https://br24.io/blog/chatgpt-entenda-como-funciona/'
+  },
+  {
+    image: <img width="100" height="100" src="https://img.icons8.com/stickers/100/literature.png" alt="Livro"/>,
+    title: 'Como otimizar sua semana de leitura: dicas práticas e eficazes',
+    description: 'Aprenda a organizar uma rotina eficaz de leitura e descobra onde encontrar livros gratuitos na internet',
+    url: 'https://dev.to/kecbm/como-otimizar-sua-semana-de-leitura-dicas-praticas-e-eficazes-480b'
+  },
+  {
+    image: <img width="100" height="100" src="https://img.icons8.com/stickers/100/online-shop.png" alt="Loja online"/>,
+    title: 'CRM de Vendas: funcionalidades e o processo de vendas realizado no Bitrix24',
+    description: 'Saiba como o Bitrix24 pode revolucionar a área de vendas da sua empresa',
+    url: 'https://br24.io/blog/controlar-meu-processo-de-vendas-com-bitrix24-crm/'
+  },
+  {
+    image: <img width="100" height="100" src="https://img.icons8.com/stickers/100/happy-mac.png" alt="Macintosh"/>,
+    title: 'Como ser uma pessoa desenvolvedora fora da curva por Sibelius Seraphini',
+    description: 'Dicas e conselhos para se destacar como desenvolvedor, abordando habilidades técnicas, mindset, aprendizado contínuo e networking',
+    url: 'https://dev.to/kecbm/como-ser-um-dev-fora-da-curva-por-sseraphini-4og2'
+  }
 ];
 
 export default blogPosts;
