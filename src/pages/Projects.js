423c0e4356139defcb70fbf0673abb4799ae7921
Klecianny Melo <kleciannymelo@gmail.com>
2023-09-08 12:32:20 -0300

    :hammer: ajustando os cards da página Projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -12,7 +12,7 @@ class Projects extends Component {
   render() {
     return (
       <div className="projects">
-        <h1 className="h1Project"><a name="projetos">PROJETOS</a></h1>
+        <h1 className="h1Project">PROJETOS</h1>
         <div className="project-list">
           {
             newProjects.map((project) => (
@@ -21,9 +21,9 @@ class Projects extends Component {
                 <h3 className="titleProject">{ project.title }</h3>
                 <p className="pProject">{ project.description }</p>
                 {
-                  project.deploy ? <button className="buttonProject"><a href={ project.deploy } target="_blank" className="linkProject">APLICAÇÃO</a></button> : null
+                  project.deploy ? <button className="buttonProject"><a href={ project.deploy } target="_blank" className="linkProject" rel="noreferrer">APLICAÇÃO</a></button> : null
                 }
-                <button className="buttonProject"><a href={ project.github } target="_blank" className="linkProject">REPOSITÓRIO</a></button>
+                <button className="buttonProject"><a href={ project.github } target="_blank" className="linkProject" rel="noreferrer">REPOSITÓRIO</a></button>
               </div>
             ))
           }
