7183bbf1b2cb6cf8f437eab1bc5a95bdb86cd562
Klecianny Melo <kleciannymelo@gmail.com>
2023-09-08 15:26:58 -0300

    :bug: adicionando a tag rel noreferrer nas tags a

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -12,11 +12,11 @@ class Contact extends Component {
     return (
       <div id="contact">
         <h1 id="title-contact">Contato</h1>
-        <a href="https://www.linkedin.com/in/kecbm/" target="_blank"><img src={ Linkedin } alt="Meu LinkedIn" height="80" width="80" className="img-contact" /></a>
-        <a href="https://github.com/Kecbm" target="_blank"><img src={ Github } alt="Meu GitHub" height="80" width="80" className="img-contact" /></a>
-        <a href="https://twitter.com/Kecbm" target="_blank"><img alt="Meu Twitter" src={ Twitter } height="80" width="80" className="img-contact" /></a>
-        <a href="https://www.instagram.com/kecbm/" target="_blank"><img alt="Meu Instagram" src={ Instagram } height="80" width="80" className="img-contact" /></a>
-        <a href="mailto:kleciannymelo@gmail.com" target="_blank"><img alt="Meu Email" src={ Gmail } height="80" width="80" className="img-contact" /></a>
+        <a href="https://www.linkedin.com/in/kecbm/" target="_blank"  rel="noreferrer"><img src={ Linkedin } alt="Meu LinkedIn" height="80" width="80" className="img-contact" /></a>
+        <a href="https://github.com/Kecbm" target="_blank"  rel="noreferrer"><img src={ Github } alt="Meu GitHub" height="80" width="80" className="img-contact" /></a>
+        <a href="https://twitter.com/Kecbm" target="_blank"  rel="noreferrer"><img alt="Meu Twitter" src={ Twitter } height="80" width="80" className="img-contact" /></a>
+        <a href="https://www.instagram.com/kecbm/" target="_blank"  rel="noreferrer"><img alt="Meu Instagram" src={ Instagram } height="80" width="80" className="img-contact" /></a>
+        <a href="mailto:kleciannymelo@gmail.com" target="_blank"  rel="noreferrer"><img alt="Meu Email" src={ Gmail } height="80" width="80" className="img-contact" /></a>
         
         <div id="third-avatar">
             <img alt="Avatar" src={thirdAvatar} />
