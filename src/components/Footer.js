f190090ab2145438e8af0d1f7617e0ee8eec1d16
Klecianny Melo <kleciannymelo@gmail.com>
2023-09-08 21:03:57 -0300

    :sparkles: icones com beat no footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,41 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../css/Footer.css';
 
-class Footer extends Component {
-  render() {
-    return (
-      <footer>
-        <h4 id="text-footer">Desenvolvido com ❤️ por <span id="name-footer">Klecianny Melo</span></h4>
-      </footer>
-    );
-  }
+const Footer = () => {
+  const icons = ['🤠', '🌵', '🐴', '🪗', '🪨', '🐮', '🫀', '🏜️', '☀️'];
+  const [currentIconIndex, setCurrentIconIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      // Mostra o próximo ícone
+      setCurrentIconIndex((prevIndex) => (prevIndex + 1) % icons.length);
+    }, 2000); // A cada 2 segundos
+
+    return () => {
+      clearInterval(interval); // Limpa o intervalo quando o componente é desmontado
+    };
+  }, []);
+
+  return (
+    <footer>
+      <h4 id="text-footer">Desenvolvido com 
+      {' '}
+      {icons.map((icon, index) => (
+          <span
+            key={index}
+            style={{ display: index === currentIconIndex ? "inline-block" : "none" }}
+            className={`icon ${index === currentIconIndex ? 'beat-animation' : ''}`}
+          >
+            {icon}
+          </span>
+      ))}
+      {' '}
+      por
+      {' '}
+        <span id="name-footer">Klecianny Melo</span>
+      </h4>
+    </footer>
+  );
 }
 
 export default Footer;
