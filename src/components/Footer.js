5f52dd47ff986d3d4420ed60ccf7da942883876e
Klecianny Melo <kleciannymelo@gmail.com>
2023-09-15 14:21:51 -0300

    :hammer: alterando o estilo para lembrar meu ambiente de desenvolvimento

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,36 +2,35 @@ import React, { useState, useEffect } from 'react';
 import '../css/Footer.css';
 
 const Footer = () => {
-  const icons = ['🤠', '🌵', '🐴', '🪗', '🪨', '🐮', '🫀', '🏜️', '☀️'];
+  const icons = ['🤠', '🌵', '🐎', '🪗', '🪨', '🫀', '🏜️', '☀️'];
   const [currentIconIndex, setCurrentIconIndex] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      // Mostra o próximo ícone
       setCurrentIconIndex((prevIndex) => (prevIndex + 1) % icons.length);
-    }, 2000); // A cada 2 segundos
+    }, 2000);
 
     return () => {
-      clearInterval(interval); // Limpa o intervalo quando o componente é desmontado
+      clearInterval(interval);
     };
-  }, []);
+  }, [icons.length]);
 
   return (
     <footer>
       <h4 id="text-footer">Desenvolvido com 
-      {' '}
-      {icons.map((icon, index) => (
-          <span
-            key={index}
-            style={{ display: index === currentIconIndex ? "inline-block" : "none" }}
-            className={`icon ${index === currentIconIndex ? 'beat-animation' : ''}`}
-          >
-            {icon}
-          </span>
-      ))}
-      {' '}
+        {' '}
+        {icons.map((icon, index) => (
+            <span
+              key={index}
+              style={{ display: index === currentIconIndex ? "inline-block" : "none" }}
+              className={`icon ${index === currentIconIndex ? 'beat-animation' : ''}`}
+            >
+              {icon}
+            </span>
+        ))}
+        {' '}
       por
-      {' '}
+        {' '}
         <span id="name-footer">Klecianny Melo</span>
       </h4>
     </footer>
