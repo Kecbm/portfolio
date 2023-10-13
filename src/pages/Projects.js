99d990ddf5767b1de135f206837e6f7ace6d8b4e
Klecianny Melo <kleciannymelo@gmail.com>
2023-10-13 14:38:49 -0300

    :sparkles: implementando botões de filtro na tela de projetos

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,36 +1,78 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import projects from '../data/Projects';
 import '../css/Projects.css';
 
-const imgStyle = {
-  height: '100px',
-  width: '100px',
-  marginTop: '0px',
-};
 
-class Projects extends Component {
-  render() {
-    return (
-      <div className="projects">
-        <h1 className="h1Project">PROJETOS</h1>
-        <div className="project-list">
-          {
-            projects.map((project) => (
-              <div key={ project.title } className={project.isSideProject ? "card side-project-card" : "card all-cards"}>
-                <img src={ project.image } alt={ project.title } style={ imgStyle }/>
-                <h3 className="titleProject">{ project.title }</h3>
-                <p className="pProject">{ project.description }</p>
-                {
-                  project.deploy ? <button className="buttonProject"><a href={ project.deploy } target="_blank" className="linkProject" rel="noreferrer">APLICAÇÃO</a></button> : null
-                }
-                <button className="buttonProject"><a href={ project.github } target="_blank" className="linkProject" rel="noreferrer">REPOSITÓRIO</a></button>
-              </div>
-            ))
-          }
-        </div>
+function Projects() {
+  const [filteredProjects, setFilteredProjects] = useState(projects);
+  const [selectedTag, setSelectedTag] = useState('todos');
+
+  const imgStyle = {
+    height: '100px',
+    width: '100px',
+    marginTop: '0px',
+  };
+  
+  const handleTagClick = (tags) => {
+    setSelectedTag(tags[0]);
+
+    if (tags.includes('todos')) {
+      setFilteredProjects(projects);
+    } else {
+      const filtered = projects.filter((project) => 
+        project.type && tags.some((tag) => project.type.includes(tag))
+      );
+
+      setFilteredProjects(filtered);
+    }
+  };
+
+  return (
+    <div className="projects">
+      <h1 className="h1Project">PROJETOS</h1>
+      <div id="btn-filter">
+        <button
+          onClick={() => handleTagClick(['todos'])}
+          className={selectedTag === 'todos' ? 'btn btn-tag-active' : 'btn  btn-tag'}
+        >
+          Todos
+        </button>
+        <button
+          onClick={() => handleTagClick(['frontend'])}
+          className={selectedTag === 'frontend' ? 'btn btn-tag-active' : 'btn  btn-tag'}
+        >
+          Frontend
+        </button>
+        <button
+          onClick={() => handleTagClick(['backend'])}
+          className={selectedTag === 'backend' ? 'btn btn-tag-active' : 'btn  btn-tag'}
+        >
+          Backend
+        </button>
+        <button
+          onClick={() => handleTagClick(['full stack'])}
+          className={selectedTag === 'full stack' ? 'btn btn-tag-active' : 'btn  btn-tag'}
+        >
+          Full Stack
+        </button>
       </div>
-    );
-  }
+      <div className="project-list">
+        {
+          filteredProjects.map((project) => (
+            <div key={ project.title } className={project.isSideProject ? "card side-project-card" : "card all-cards"}>
+              <img src={ project.image } alt={ project.title } style={ imgStyle }/>
+              <h3 className="titleProject">{ project.title }</h3>
+              <p className="pProject">{ project.description }</p>
+              {
+                project.deploy ? <button className="buttonProject"><a href={ project.deploy } target="_blank" className="linkProject" rel="noreferrer">APLICAÇÃO</a></button> : null
+              }
+              <button className="buttonProject"><a href={ project.github } target="_blank" className="linkProject" rel="noreferrer">REPOSITÓRIO</a></button>
+            </div>
+          ))
+        }
+      </div>
+    </div>
+  );
 }
 
 export default Projects;
