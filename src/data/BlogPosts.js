f34663f108188b7aedc5aa66ab52064966fc9093
Klecianny Melo <kleciannymelo@gmail.com>
2023-10-13 13:06:15 -0300

    :hammer: ajustando os cards dá página de blog posts

--- src/data/BlogPosts.js
+++ src/data/BlogPosts.js
@@ -1,27 +1,41 @@
 const blogPosts = [
   {
-    image: "https://img.icons8.com/stickers/100/bot.png",
+    image: 'https://img.icons8.com/stickers/100/untested.png',
+    title: 'Cronograma de projeto',
+    description: 'O artigo aborda a importância de um cronograma de projeto, fornecendo orientações práticas para sua elaboração',
+    url: 'https://br24.io/blog/cronograma-de-projeto/',
+    location: 'Br24'
+  },
+  {
+    image: 'https://img.icons8.com/stickers/100/bot.png',
     title: 'ChatGPT',
     description: 'Entenda o que é, como funciona, limitações, ferramentas e o que esperar da nova atualização no GPT-5',
     url: 'https://br24.io/blog/chatgpt-entenda-como-funciona/',
     location: 'Br24'
   },
   {
-    image: "https://img.icons8.com/stickers/100/literature.png",
+    image: 'https://img.icons8.com/stickers/100/literature.png',
     title: 'Como otimizar sua semana de leitura',
     description: 'Aprenda a organizar uma rotina eficaz de leitura e descobra onde encontrar livros gratuitos na internet',
     url: 'https://dev.to/kecbm/como-otimizar-sua-semana-de-leitura-dicas-praticas-e-eficazes-480b',
     location: 'Dev.to'
   },
   {
-    image: "https://img.icons8.com/stickers/100/online-shop.png",
+    image: 'https://img.icons8.com/stickers/100/online-shop.png',
     title: 'CRM de Vendas',
     description: 'Saiba o que é um CRM de vendas, suas principais funcionalidades e conheça o Bitrix24, o melhor CRM do mercado',
     url: 'https://br24.io/blog/controlar-meu-processo-de-vendas-com-bitrix24-crm/',
     location: 'Br24'
   },
   {
-    image: "https://img.icons8.com/stickers/100/happy-mac.png",
+    image: 'https://img.icons8.com/stickers/100/graph.png',
+    title: 'KPIs',
+    description: 'Entenda a importância dos KPIs, como defini-los e utiliza-los efetivamente para impulsionar o sucesso dos negócios',
+    url: 'https://br24.io/blog/kpis/',
+    location: 'Br24'
+  },
+  {
+    image: 'https://img.icons8.com/stickers/100/happy-mac.png',
     title: 'Como ser uma pessoa desenvolvedora fora da curva',
     description: 'Dicas e conselhos para se destacar como desenvolvedor, com um dos ícones da bolha dev, Sibelius Seraphini',
     url: 'https://dev.to/kecbm/como-ser-um-dev-fora-da-curva-por-sseraphini-4og2',
