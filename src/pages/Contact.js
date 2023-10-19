971ac3122b923e80335a21224cec875b841cd2a9
Klecianny Melo <kleciannymelo@gmail.com>
2023-10-19 16:37:01 -0300

    :sparkles: adicionando o link para meu perfil na comunidade do Figma

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -13,7 +13,8 @@ class Contact extends Component {
       <div id="contact">
         <h1 id="title-contact">Contato</h1>
         <a href="https://www.linkedin.com/in/kecbm/" target="_blank"  rel="noreferrer"><img src={ Linkedin } alt="Meu LinkedIn" height="80" width="80" className="img-contact" /></a>
-        <a href="https://github.com/Kecbm" target="_blank"  rel="noreferrer"><img src={ Github } alt="Meu GitHub" height="80" width="80" className="img-contact" /></a>
+        <a href="https://github.com/Kecbm" target="_blank"  rel="noreferrer"><img alt="Meu GitHub" src={ Github } height="80" width="80" className="img-contact" /></a>
+        <a href="https://www.figma.com/@kecbm" target="_blank"  rel="noreferrer"><img alt="Meu Figma" src="https://img.icons8.com/stickers/100/figma.png" height="80" width="80" className="img-contact" /></a>
         <a href="https://twitter.com/Kecbm" target="_blank"  rel="noreferrer"><img alt="Meu Twitter" src={ Twitter } height="80" width="80" className="img-contact" /></a>
         <a href="https://www.instagram.com/kecbm/" target="_blank"  rel="noreferrer"><img alt="Meu Instagram" src={ Instagram } height="80" width="80" className="img-contact" /></a>
         <a href="mailto:kleciannymelo@gmail.com" target="_blank"  rel="noreferrer"><img alt="Meu Email" src={ Gmail } height="80" width="80" className="img-contact" /></a>
