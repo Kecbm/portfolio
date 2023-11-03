9d9350d398d28358a0de27372ebf3aafa818d2bd
Klecianny Melo <kleciannymelo@gmail.com>
2023-11-03 12:09:46 -0300

    :sparkles: adicionando imagens dos cursos de formação

--- src/data/Formations.js
+++ src/data/Formations.js
@@ -1,32 +1,39 @@
+import admSf from '../assets/1.SF.jpeg';
+import trybe from '../assets/2.Trybe.png';
+import devSf from '../assets/3.SF.png';
+import comunication from '../assets/4.Comunicação.jpeg';
+import onbB24 from '../assets/5.B24.png';
+import fundB24 from '../assets/6.B24.jpeg';
+
 const formations = [
     {
         title: 'Fundamentos do Bitrix24',
-        url: 'https://media.licdn.com/dms/image/D4D22AQHGkYGckwnfeA/feedshare-shrink_1280/0/1696255385668?e=1700092800&v=beta&t=-Ei6zqmkNI4Z9QK3z8I7tM1cWvfH7aS-A2kFi1yAnTA',
+        url: fundB24,
         informations: 'Maio de 2023'
     },
     {
         title: 'Implementando o Bitrix.CRM - Onboarding',
-        url: 'https://media.licdn.com/dms/image/D4E22AQE7s5foZikIUw/feedshare-shrink_800/0/1690557087405?e=1700092800&v=beta&t=wBzudNDqWJI5QTzyN2vtWn_UinbQLHwfceS2vmuN1xY',
+        url: onbB24,
         informations: 'Maio de 2023'
     },
     {
         title: 'Comunicação Assertiva - By Conquer',
-        url: 'https://media.licdn.com/dms/image/C4D22AQH-itEbVeOj3A/feedshare-shrink_800/0/1676647979368?e=1700092800&v=beta&t=FMtboJdEzd6whswhKUTffkJihTffL6JMZAhmYN1-LNg',
+        url: comunication,
         informations: 'Janeiro de 2023'
     },
     {
         title: 'Salesforce Development TRaining for Beginners',
-        url: 'https://media.licdn.com/dms/image/sync/C4D27AQHqGgFCH1zEvg/articleshare-shrink_800/0/1694466783044?e=1697835600&v=beta&t=Y5neDfsGa8PpKcMlQdwh1vz3kfzvnm6aGCYfe1UMlR8',
+        url: devSf,
         informations: 'Janeiro de 2023 - 14 horas'
     },
     {
         title: 'Formação em desenvolvimento Web',
-        url: 'https://media.licdn.com/dms/image/sync/C4D27AQHXNgKAo-10ow/articleshare-shrink_800/0/1697068235580?e=1697835600&v=beta&t=mRwJsENpgva86uFgnq7STiOm6fALz7GlOZZR_GNmYC8',
+        url: trybe,
         informations: 'Dezembro de 2022 - 12 meses'
     },
     {
       title: 'Curso Administrador Salesforce iniciante',
-      url: 'https://media.licdn.com/dms/image/C4D22AQHwPK71lOxxQg/feedshare-shrink_800/0/1671193323653?e=1700092800&v=beta&t=IflnnKx_YsHAfoRGf4clq9ZvuNysNaT7FUkRpJj8Hs0',
+      url: admSf,
       informations: 'Novembro de 2022 - 3.5 horas'
     }
   ];
