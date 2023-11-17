b8728696a8a12d518080cfe500ac189531449c25
Klecianny Melo <kleciannymelo@gmail.com>
2023-11-17 14:28:56 -0300

    :sparkles: translate the Blog Post page

--- src/data/BlogPosts.js
+++ src/data/BlogPosts.js
@@ -1,52 +1,94 @@
 const blogPosts = [
   {
     image: 'https://img.icons8.com/stickers/100/book-shelf.png',
-    title: 'Playbook: o que é, como aplicar, funcionalidades e vantagens',
     url: 'https://br24.io/blog/playbook/',
     location: 'Br24',
-    date: 'Outubro de 2023'
+    en: {
+      title: 'Playbook: what it is, how to apply it, features and advantages',
+      date: 'October 2023'
+    },
+    pt: {
+      title: 'Playbook: o que é, como aplicar, funcionalidades e vantagens',
+      date: 'Outubro de 2023'
+    }
   },
   {
     image: 'https://img.icons8.com/stickers/100/group-of-projects.png',
-    title: 'Cronograma de Projeto: o que é, como funciona, como montar e vantagens',
     url: 'https://br24.io/blog/cronograma-de-projeto/',
     location: 'Br24',
-    date: 'Setembro de 2023'
+    en: {
+      title: 'Project Schedule: what it is, how it works, how to set it up and advantages',
+      date: 'September 2023'
+    },
+    pt: {
+      title: 'Cronograma de Projeto: o que é, como funciona, como montar e vantagens',
+      date: 'Setembro de 2023'
+    }
   },
   {
     image: 'https://img.icons8.com/stickers/100/graph.png',
-    title: 'KPIs: o que são, para que servem, vantagens e dicas de como aplicar',
     url: 'https://br24.io/blog/kpis/',
     location: 'Br24',
-    date: 'Agosto de 2023'
+    en: {
+      title: 'KPIs: what they are, what they are for, advantages and tips on how to apply',
+      date: 'August 2023'
+    },
+    pt: {
+      title: 'KPIs: o que são, para que servem, vantagens e dicas de como aplicar',
+      date: 'Agosto de 2023'
+    }
   },
   {
     image: 'https://img.icons8.com/stickers/100/chatbot.png',
-    title: 'O que é ChatGPT: benefícios, como funciona e atualizações GPT-5',
     url: 'https://br24.io/blog/chatgpt-entenda-como-funciona/',
     location: 'Br24',
-    date: 'Julho de 2023'
+    en: {
+      title: 'What is ChatGPT: benefits, how it works and GPT-5 updates',
+      date: 'July 2023'
+    },
+    pt: {
+      title: 'O que é ChatGPT: benefícios, como funciona e atualizações GPT-5',
+      date: 'Julho de 2023'
+    }
   },
   {
     image: 'https://img.icons8.com/stickers/100/books-1.png',
-    title: 'Como otimizar sua semana de leitura: dicas práticas e eficazes',
     url: 'https://dev.to/kecbm/como-otimizar-sua-semana-de-leitura-dicas-praticas-e-eficazes-480b',
     location: 'Dev.to',
-    date: 'Junho de 2023'
+    en: {
+      title: 'How to optimize your reading week: practical and effective tips',
+      date: 'June 2023'
+    },
+    pt: {
+      title: 'Como otimizar sua semana de leitura: dicas práticas e eficazes',
+      date: 'Junho de 2023'
+    }
   },
   {
     image: 'https://img.icons8.com/stickers/100/best-sales.png',
-    title: 'CRM de Vendas: benefícios, como utilizar e diferenciais do CRM Bitrix24',
     url: 'https://br24.io/blog/controlar-meu-processo-de-vendas-com-bitrix24-crm/',
     location: 'Br24',
-    date: 'Junho de 2023'
+    en: {
+      title: 'Sales CRM: benefits, how to use and differences of CRM Bitrix24',
+      date: 'June 2023'
+    },
+    pt: {
+      title: 'CRM de Vendas: benefícios, como utilizar e diferenciais do CRM Bitrix24',
+      date: 'Junho de 2023'
+    }
   },
   {
     image: 'https://img.icons8.com/stickers/100/my-computer.png',
-    title: 'Como ser uma pessoa desenvolvedora fora da curva',
     url: 'https://dev.to/kecbm/como-ser-um-dev-fora-da-curva-por-sseraphini-4og2',
     location: 'Dev.to',
-    date: 'Abril de 2022'
+    en: {
+      title: 'How to be an out-of-the-box developer',
+      date: 'April 2022'
+    },
+    pt: {
+      title: 'Como ser uma pessoa desenvolvedora fora da curva',
+      date: 'Abril de 2022'
+    }
   }
 ];
 
--- src/pages/BlogPosts.js
+++ src/pages/BlogPosts.js
@@ -1,33 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
 import blogPosts from '../data/BlogPosts';
 import '../css/BlogPosts.css';
+import { useLanguage } from '../context/LanguageContext';
+import Translations from '../locales/Translations';
 
-class BlogPosts extends Component {
-  render() {
-    return (
-      <div className="blogposts">
-        <h1 className="h1Blog">BLOG POSTS</h1>
-        <div className="blog-list">
-          {
-            blogPosts.map((post) => (
-              <div key={ post.title } className={"card-blog"}>
-                {/* <img src={ post.location === "Br24" ? "https://br24.io/wp-content/uploads/2022/09/Logo-Site-Br24-222x59-1.png" : "https://dev-to-uploads.s3.amazonaws.com/uploads/logos/resized_logo_UQww2soKuUsjaOGNB38o.png" } alt={ post.location } className="tag-location" /> */}
-                <img src={ post.image } alt={ post.title } className="img-card-blog"/>
-                <h3 className="titleBlog">{ post.title }</h3>
-                {/* <p className="pBlog">{ post.description }</p> */}
-                <button className="buttonBlog"><a href={ post.url } target="_blank" className="linkBlog" rel="noreferrer"><img width="30" height="30" src="https://img.icons8.com/ios-glyphs/30/0c0d11/overview-pages-1.png" alt="overview-pages-1"/></a></button>
-                <div id="line"></div>
-                <div id="footer-blog">
-                  <img width="30" height="30" src="https://img.icons8.com/ios-glyphs/30/calendar.png" alt="calendar"/>
-                  <p className="date-blog">{post.date}</p>
-                </div>
+const BlogPosts = () => {
+  const { language } = useLanguage();
+
+  return (
+    <div className="blogposts">
+      <h1 className="h1Blog">BLOG POSTS</h1>
+      <div className="blog-list">
+        {
+          blogPosts.map((post) => (
+            <div key={ post[language].title } className={"card-blog"}>
+              {/* <img src={ post.location === "Br24" ? "https://br24.io/wp-content/uploads/2022/09/Logo-Site-Br24-222x59-1.png" : "https://dev-to-uploads.s3.amazonaws.com/uploads/logos/resized_logo_UQww2soKuUsjaOGNB38o.png" } alt={ post.location } className="tag-location" /> */}
+              <img src={ post.image } alt={ post[language].title } className="img-card-blog"/>
+              <h3 className="titleBlog">{ post[language].title }</h3>
+              {/* <p className="pBlog">{ post.description }</p> */}
+              <button className="buttonBlog"><a href={ post.url } target="_blank" className="linkBlog" rel="noreferrer"><img width="30" height="30" src="https://img.icons8.com/ios-glyphs/30/0c0d11/overview-pages-1.png" alt="overview-pages-1"/></a></button>
+              <div id="line"></div>
+              <div id="footer-blog">
+                <img width="30" height="30" src="https://img.icons8.com/ios-glyphs/30/calendar.png" alt="calendar"/>
+                <p className="date-blog">{post[language].date}</p>
               </div>
-            ))
-          }
-        </div>
+            </div>
+          ))
+        }
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default BlogPosts;
