49788493f97943b7743f00d56d6e2f23b7f5ce15
Klecianny Melo <kleciannymelo@gmail.com>
2023-11-23 16:33:11 -0300

    :hammer: adjusting im my about informations

--- src/locales/Translations.js
+++ src/locales/Translations.js
@@ -10,7 +10,7 @@ const translations = {
 
         // About
         aboutTitle: 'About',
-        aboutText: 'Hello, I am Klecianny Melo, I am 27 years old and I live in Garanhuns, Pernambuco. I am passionate about technology and believe in the power of education to transform lives. In addition, I practice cycling and crossfit to stay healthy, I like reading books from different categories and playing video games. I actively participate in the Bolha Dev on Twitter, a community of developers who share knowledge and experiences about the technological universe.',
+        aboutText: 'Hello, I am Klecianny Melo, I am 27 years. I am passionate about technology and believe in the power of education to transform lives. In addition, I practice cycling and crossfit to stay healthy, I like reading books from different categories and playing video games. I actively participate in the Bolha Dev on Twitter, a community of developers who share knowledge and experiences about the technological universe.',
         aboutTitle_1: 'Professional performance',
         about_list: 'Full Stack PHP Developer',
         about_list_1: 'Saxophonist at the Manoel Rabelo Orchestra',
@@ -38,7 +38,7 @@ const translations = {
 
         // About
         aboutTitle: 'Sobre',
-        aboutText: 'Olá, Eu sou Klecianny Melo, tenho 27 anos e moro em Garanhuns, Pernambuco. Sou apaixonada por tecnologia e acredito no poder da educação para transformar vidas. Além disso, pratico ciclismo e crossfit para me manter saudável, gosto de ler livros de diversas categorias e jogar videogame. Participo ativamente da Bolha Dev no Twitter, uma comunidade de desenvolvedores que compartilha conhecimentos e experiências sobre o universo tecnológico.',
+        aboutText: 'Olá, Eu sou Klecianny Melo, tenho 27 anos. Sou apaixonada por tecnologia e acredito no poder da educação para transformar vidas. Além disso, pratico ciclismo e crossfit para me manter saudável, gosto de ler livros de diversas categorias e jogar videogame. Participo ativamente da Bolha Dev no Twitter, uma comunidade de desenvolvedores que compartilha conhecimentos e experiências sobre o universo tecnológico.',
         aboutTitle_1: 'Atuação Profissional',
         about_list: 'Desenvolvedora PHP Full Stack',
         about_list_1: 'Saxofonista na Orquestra Manoel Rabelo',
