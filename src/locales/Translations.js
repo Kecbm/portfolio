22b356e1f859990c92eae82fac4ab61bb17d52ee
Klecianny Melo <kleciannymelo@gmail.com>
2024-03-02 15:32:27 -0300

    :sparkles: new text about me

--- src/locales/Translations.js
+++ src/locales/Translations.js
@@ -10,7 +10,7 @@ const translations = {
 
         // About
         aboutTitle: 'About',
-        aboutText: 'Hello, I am Klecianny Melo, I am 27 years. I am passionate about technology and believe in the power of education to transform lives. In addition, I practice cycling and crossfit to stay healthy, I like reading books from different categories and playing video games. I actively participate in the Bolha Dev on Twitter, a community of developers who share knowledge and experiences about the technological universe.',
+        aboutText: 'Hello, I am Klecianny Melo. I focus my efforts on driving code quality and leading innovative initiatives, such as encouraging the use of Codeium AI. I actively share knowledge through articles on the Br24 blog and on my Dev.to profile. Outside of the technological world, I cycle to stay healthy. My journey is one of continuous learning and collaboration in the development community.',
         aboutTitle_1: 'Professional performance',
         about_list: 'Full Stack PHP Developer',
         about_list_1: 'Saxophonist at the Manoel Rabelo Orchestra',
@@ -38,7 +38,7 @@ const translations = {
 
         // About
         aboutTitle: 'Sobre',
-        aboutText: 'Olá, Eu sou Klecianny Melo, tenho 27 anos. Sou apaixonada por tecnologia e acredito no poder da educação para transformar vidas. Além disso, pratico ciclismo e crossfit para me manter saudável, gosto de ler livros de diversas categorias e jogar videogame. Participo ativamente da Bolha Dev no Twitter, uma comunidade de desenvolvedores que compartilha conhecimentos e experiências sobre o universo tecnológico.',
+        aboutText: 'Olá, Eu sou Klecianny Melo. Concentro meus esforços em impulsionar a qualidade do código e liderar iniciativas inovadoras, como o incentivo da utilização da IA Codeium. Compartilho ativamente conhecimentos através de artigos no blog da Br24 e no meu perfil do Dev.to. Fora do mundo tecnológico, pratico ciclismo para me manter saudável. Minha jornada é de aprendizado contínuo e colaboração na comunidade de desenvolvimento.',
         aboutTitle_1: 'Atuação Profissional',
         about_list: 'Desenvolvedora PHP Full Stack',
         about_list_1: 'Saxofonista na Orquestra Manoel Rabelo',
