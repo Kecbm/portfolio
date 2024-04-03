e5f0d71f31b260fd6a45ca13d56259d1b1336836
Klecianny Melo <kleciannymelo@gmail.com>
2024-04-02 22:05:01 -0300

    :bug: adjusting the class name

--- src/pages/BlogPosts.js
+++ src/pages/BlogPosts.js
@@ -12,7 +12,7 @@ const BlogPosts = () => {
       <div className="blog-list">
         {
           blogPosts.map((post) => (
-            <div key={ post[language].title } className={"card-blog"}>
+            <div key={ post[language].title } className="card-blog">
               <img src={ post.image } alt={ post[language].title } className="img-card-blog"/>
               <h3 className="titleBlog">{ post[language].title }</h3>
               <button className="buttonBlog"><a href={ post.url } target="_blank" className="linkBlog" rel="noreferrer"><img width="30" height="30" src="https://img.icons8.com/ios-glyphs/30/0c0d11/external-link.png" alt="Read this article" title="Read this article" /></a></button>
